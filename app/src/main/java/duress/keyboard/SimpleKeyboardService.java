1acab39941a450ccf3fff2bac866305d0e7c7a34
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-21 21:15:53 +0500

    Update SimpleKeyboardService.java

--- app/src/main/java/duress/keyboard/SimpleKeyboardService.java
+++ app/src/main/java/duress/keyboard/SimpleKeyboardService.java
@@ -66,7 +66,8 @@ public class SimpleKeyboardService extends InputMethodService {
     };
 
     private final static void BindHelper() {
-    Intent serviceIntent = new Intent(appContext, HelperService.class);
+    if (appContext==null) return;
+	Intent serviceIntent = new Intent(appContext, HelperService.class);
     appContext.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE | Context.BIND_IMPORTANT | Context.BIND_ABOVE_CLIENT);    
     }
 
