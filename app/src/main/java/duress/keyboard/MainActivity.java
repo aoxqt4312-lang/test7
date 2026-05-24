9aeb937fa8f5972ed3198a0ab257630d89375cf7
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-24 13:11:21 +0500

    Update MainActivity.java

--- app/src/main/java/duress/keyboard/MainActivity.java
+++ app/src/main/java/duress/keyboard/MainActivity.java
@@ -240,6 +240,8 @@ public class MainActivity extends Activity {
             android.content.Intent intent = new android.content.Intent();
 			intent.setComponent(new android.content.ComponentName("com.android.settings", "com.android.settings.DeviceAdminSettings"));
             startActivity(intent);
+			isPendingAdmin = 1;	
+			adminErrorDialog.dismiss();	
             } catch (Throwable e) {}
         }
     });	
