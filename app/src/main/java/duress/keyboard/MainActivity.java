d65f1fddbaa4941ddf5212251ec430bfb7cef5d2
aoxqt4312-lang <aoxqt4312@gmail.com>
2026-05-24 17:02:44 +0500

    Update MainActivity.java

--- app/src/main/java/duress/keyboard/MainActivity.java
+++ app/src/main/java/duress/keyboard/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends Activity {
 
 	private android.app.AlertDialog accessibilityDialog;
 	private android.app.AlertDialog adminErrorDialog;
+	private android.app.AlertDialog infoDialog;
 	private android.app.AlertDialog AdditionalOptionsWarning;
 	private static boolean main=true;
 	boolean accessibilityEnabled = false;
